c92636057c9a3c75459eb9a9f6653ff9d5f36d53
Lai-YT <381xvmvbib@gmail.com>
2023-04-18 09:43:03 +0800

    docs: add comment to test utils

--- test/util.h
+++ test/util.h
@@ -3,8 +3,10 @@
 
 #include <stddef.h>
 
+/// @brief A test case should take no arguments and has no return value.
 typedef void (*TestCase)();
 
+/// @return 0 if all tests passed with out aborting error, 1 otherwise.
 int run_all_tests(TestCase cases[], size_t num_of_cases);
 
 #endif /* end of include guard: TEST_UTIL_H */
