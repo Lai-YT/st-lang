95ec55c377179b26b08bcfbe333c9520b726ba33
Lai-YT <381xvmvbib@gmail.com>
2023-04-19 12:44:35 +0800

    feat: lexer takes filename as argument
    Better command-line interface.
    Symbol dumping is now optional with flag `--dump`.
    
    BREAKING CHANGE: pass the file to the lexer with `./lexer file.t`
    instead of `./lexer < file.t`

---
+++ lexer/lextest.c
@@ -0,0 +1,77 @@
+#include <getopt.h>
+#include <stdbool.h>
+#include <stddef.h>  // NULL
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "list.h"
+#include "symtab.h"
+
+// the symbol table used in the lexer
+SymbolTable* symtab;
+// the flex scanning routine
+extern int yylex();
+// whenever `yylex` is called, it scans tokens from the global input file yyin
+extern FILE* yyin;
+
+void dump_symbols(SymbolTable*);
+
+int main(int argc, char* argv[]) {
+  struct option options[] = {
+      {"dump", no_argument, 0, 'd'},
+  };
+
+  bool symbol_dump = false;
+
+  int arg;
+  while ((arg = getopt_long(argc, argv, "d", options, NULL)) != -1) {
+    switch (arg) {
+      case 'd':
+        symbol_dump = true;
+        break;
+      default:
+        fprintf(stderr, "usage: %s [-d] FILE\n", argv[0]);
+        exit(EXIT_FAILURE);
+    }
+  }
+
+  if (optind + 1 != argc) {
+    fprintf(stderr, "usage: %s [-d] FILE\n", argv[0]);
+    exit(EXIT_FAILURE);
+  }
+
+  yyin = fopen(argv[optind], "r");
+  if (!yyin) {
+    fprintf(stderr, "error: could not open input file %s\n", argv[optind]);
+    exit(EXIT_FAILURE);
+  }
+
+  // a new symbol table for the lexer to use
+  symtab = symtab_create();
+
+  // Since we're not using `return` after recognizing a token, `yylex` lexes the
+  // entire file in a single call.
+  yylex();
+
+  if (symbol_dump) {
+    puts("");
+    dump_symbols(symtab);
+  }
+
+  symtab_delete(symtab);
+  fclose(yyin);
+
+  return 0;
+}
+
+void dump_symbols(SymbolTable* table) {
+  puts("Symbol Table:");
+  List* dump = symtab_dump(symtab);
+  List* curr = dump;
+  while (curr) {
+    Entry* entry = curr->val;
+    printf("%s: %s\n", entry->name, type_to_str(entry->type));
+    curr = curr->rest;
+  }
+  list_delete(dump);
+}
