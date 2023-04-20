09af284b9bbf73f157c198e98728a0ab66a98344
Lai-YT <381xvmvbib@gmail.com>
2023-04-20 16:16:31 +0800

    optimize: use hash table instead of list

--- src/symtab.c
+++ src/symtab.c
@@ -1,12 +1,13 @@
 #include "symtab.h"
 
 #include <stdlib.h>
-#include <string.h>
+#include <string.h>  // strcpy
 
+#include "hashtab.h"
 #include "list.h"
 
 struct SymbolTable {
-  List* entries;
+  HashTable* entries;
 };
 
 static Entry* entry_create(const char* name, Type type) {
@@ -22,20 +23,12 @@ static inline void entry_delete(Entry* entry) {
 
 SymbolTable* symtab_create() {
   SymbolTable* table = malloc(sizeof(SymbolTable));
-  table->entries = NULL;
+  table->entries = hashtab_create();
   return table;
 }
 
 Entry* symtab_lookup(SymbolTable* table, const char* name) {
-  List* curr = table->entries;
-  while (curr) {
-    Entry* entry = curr->val;
-    if (strcmp(entry->name, name) == 0) {
-      return entry;
-    }
-    curr = curr->rest;
-  }
-  return NULL;
+  return hashtab_search(table->entries, name);
 }
 
 Entry* symtab_insert(SymbolTable* table, const char* name, Type type) {
@@ -44,29 +37,24 @@ Entry* symtab_insert(SymbolTable* table, const char* name, Type type) {
     return entry;
   }
   entry = entry_create(name, type);
-  table->entries = list_create(entry, table->entries);
+  hashtab_insert(table->entries, name, entry);
   return entry;
 }
 
 List* symtab_dump(SymbolTable* table) {
   // shallow copying the list
   List* dump = NULL;
-  List* curr = table->entries;
-  while (curr) {
-    dump = list_create(curr->val, dump);
-    curr = curr->rest;
-  }
+  HASHTAB_ITR_FOR_EACH(table->entries, itr,
+                       { dump = list_create(hashtab_itr_value(itr), dump); });
   return dump;
 }
 
 void symtab_delete(SymbolTable* table) {
-  List* curr = table->entries;
-  while (curr) {
-    List* tmp = curr;
-    curr = curr->rest;
-    entry_delete(tmp->val);
-    free(tmp);
-  }
+  HASHTAB_ITR_FOR_EACH(table->entries, itr, {
+    Entry* entry = hashtab_itr_value(itr);
+    entry_delete(entry);
+  });
+  hashtab_delete(table->entries);
   free(table);
 }
 
